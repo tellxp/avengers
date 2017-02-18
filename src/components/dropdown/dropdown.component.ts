2443e2fa365b3e80c7d6568dd330e6e3b32c7b94
tellxp <xiao.peng@live.cn>
2017-02-18 22:21:41 +0800

    为dropdown添加了一个简单的淡入淡出动画

--- src/components/dropdown/dropdown.component.ts
+++ src/components/dropdown/dropdown.component.ts
@@ -1,10 +1,28 @@
-import {Component, OnInit, Input, AfterViewInit, ViewChild, ElementRef} from '@angular/core';
+import {
+  Component, OnInit, Input, AfterViewInit, ViewChild, ElementRef, trigger, state,
+  transition, style, animate
+} from '@angular/core';
 import {DomService} from "../common/dom.service";
 
 @Component({
   selector: 'ave-dropdown',
   templateUrl: './dropdown.component.html',
   styleUrls: ['./dropdown.component.scss'],
+  animations: [
+    trigger('expandedState', [
+      transition('void => *', [
+        style({
+          opacity: 0
+        }),
+        animate('0.5s ease-in')
+      ]),
+      transition('* => void', [
+        animate('0.5s ease-out', style({
+          opacity: 0
+        }))
+      ])
+    ])
+  ],
   providers: [DomService]
 })
 export class Dropdown implements OnInit, AfterViewInit {
